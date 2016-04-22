81f1044426ecd51734150bf6b030e0e03003620f
Andreas Klein <mail@andreasklein.org>
2016-04-22 17:09:59 +0200

    Use port from heroku env

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var http2 = require('http2');
 
+var port = process.env.PORT || 9000;
+
 var options = {
-	key: fs.readFileSync('./cert.com.key'),
-	cert: fs.readFileSync('./cert.com.crt')
+    key: fs.readFileSync('./cert.com.key'),
+    cert: fs.readFileSync('./cert.com.crt')
 };
 
 http2.createServer(options, function(req, res) {
-	res.end('Welcome HTTP/2.0');
-}).listen(8000);
+    res.end('Welcome HTTP/2.0');
+}).listen(port);
 
-console.log("Server listening on Port 8000");
+console.log('Server listening on Port %s', port);
